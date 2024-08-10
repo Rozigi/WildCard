3acb27f864f262b28463608545dd992912e79dcd
Rozigi <reozigi2005@gmail.com>
2024-08-09 22:52:44 -0500

    Add files via upload

---
+++ SQLQueryWildCartAndFunctions.sql
@@ -0,0 +1,93 @@
+/**Friday 9th of August 2024.
+---Cover 
+--- 1, Add more Functions.
+---2, Cover temporary Tables and Global Tables.
+---3, CTE 
+---4, Date Functions.
+---5, Expand more on StoreProcedure and Parameters.
+---6, SQL Joins.
+---7, ERD.
+
+---Assighnment for Tuesday 6th of August 2024
+---Watch Youtube Videos on Data warehouse vs Data Mart.
+---Watch Videos on DataBase Index.
+**/
+
+USE SQLTrainingCase
+SELECT  [EmployeeID]
+	,UPPER (LastName+ ' '+FirstName+' '+MiddleName)	AS EmployeeName
+      ,[LastName]
+      ,[FirstName]
+      ,[MiddleName]
+      ,[JobID]
+      ,[ManagerID]
+      ,[HireDate]
+      ,[Salary]
+      ,[Comm]
+      ,[DepartmentID]
+	  INTO NewEmployee
+  FROM [SQLTrainingCase].[dbo].[Employee]
+
+SELECT * 
+FROM NewEmployee
+
+SELECT  [EmployeeID]
+,Substring(EmployeeName, 1, 8) As EMpName_String
+      ,[LastName]
+      ,[FirstName]
+      ,[MiddleName]
+      ,[JobID]
+      ,[ManagerID]
+      ,[HireDate]
+      ,[Salary]
+      ,[Comm]
+      ,[DepartmentID]
+  FROM [SQLTrainingCase].[dbo].[NewEmployee]
+
+	---TEMP TABLES
+	----To Creat a Temp Table add #
+	---These table are accessible in the current session
+
+SELECT  [EmployeeID]
+,Substring(LastName, 1, 4) As LastName_String
+      ,[LastName]
+      ,[FirstName]
+      ,[MiddleName]
+      ,[JobID]
+      ,[ManagerID]
+      ,[HireDate]
+      ,[Salary]
+      ,[Comm]
+      ,[DepartmentID]
+	   INTO #Employee_String
+  FROM [SQLTrainingCase].[dbo].[Employee]
+
+  --Check Temp Table 
+  SELECT * 
+  FROM  #Employee_String
+
+  ---Global TABLES
+	----To Creat a Temp Table add 2 ##
+	---These table are accessible in the other session
+
+	
+SELECT  [EmployeeID]
+,Substring(LastName, 1, 4) As LastName_String
+      ,[LastName]
+      ,[FirstName]
+      ,[MiddleName]
+      ,[JobID]
+      ,[ManagerID]
+      ,[HireDate]
+      ,[Salary]
+      ,[Comm]
+      ,[DepartmentID]
+	  INTO ##Employee_String
+  FROM [SQLTrainingCase].[dbo].[Employee]
+
+  
+  --Check Temp Table 
+  SELECT * 
+  FROM  #Employee_String
+
+
